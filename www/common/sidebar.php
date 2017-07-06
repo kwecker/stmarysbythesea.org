62636243abf631701cd736d8ed1c758e48f28515
Kevin Wecker <kwecker@gmail.com>
2017-07-06 13:15:17 -0700

    Catch up

--- www/common/sidebar.php
+++ www/common/sidebar.php
@@ -1,136 +1,99 @@
 <div class="side_bar">
 
-	<!-- Services -->
-	<div id="services">
-		<h2>Sunday Services</h2>
-		<h3>8:00am</h3>
-		<ul>
-			<li>Rite I Eucharist</li>
-		</ul>
-		<p>
-			<em>Please join us for coffee, tea, treats and conversation after the service</em>
-		</p>
-		<div class="hr"><hr></div>
-		<h3>10:00am</h3>
-		<ul>
-			<li>Rite II Choral Eucharist (Contemporary Language)</li>
-			<li>Sunday School: Toddler Room, Godly Play(September&ndash;May), and Youth Group</li>
-		</ul>
-		<p>
-			<em>Please join us for coffee, tea, treats and conversation after the service</em>
-		</p>
-	</div>
-	<!-- End Services -->
+    <!-- Services -->
+    <div id="services">
+        <h2>Sunday Services</h2>
+        <h3>8:00am</h3>
+        <ul>
+            <li>Rite I Eucharist</li>
+        </ul>
+        <p>
+            <em>Please join us for coffee, tea, treats and conversation after the service</em>
+        </p>
+        <div class="hr"><hr></div>
+        <h3>10:00am</h3>
+        <ul>
+            <li>Rite II Choral Eucharist (Contemporary Language)</li>
+            <li>Sunday School: Toddler Room, Godly Play(September&ndash;May), and Youth Group</li>
+        </ul>
+        <p>
+            <em>Please join us for coffee, tea, treats and conversation after the service</em>
+        </p>
+    </div>
+    <!-- End Services -->
 
-	<!-- Special Services -->
-	<div class="hr hr_large"><hr class="large_dots"></div>
+    <!-- Special Services -->
+    <!-- <div class="hr hr_large"><hr class="large_dots"></div> -->
+    <!-- End Special Services -->
 
-	<div class="special-services">
-		<h2>Holy Week 2017</h2>
-		<h3>Palm Sunday, April 9th</h3>
-		<ul>
-			<li><strong>7:30am:</strong> Holy Eucharist, Rite I</li>
-			<li><strong>9:30am:</strong> The Liturgy of the Palms<br>&amp; Procession/Berwick Park</li>
-			<li><strong>10:00am:</strong> Holy Eucharist, Rite II</li>
-		</ul>
-		<div class="hr"><hr></div>
-		<h3>Maundy Thursday, April 13th</h3>
-		<ul>
-			<li><strong>6:00pm:</strong> Soup Supper</li>
-			<li><strong>7:00pm:</strong> Holy Eucharist;<br>Washing of Feet;<br>Stripping of the Altar</li>
-		</ul>
-		<div class="hr"><hr></div>
-		<h3>Good Friday, April 14th</h3>
-		<ul>
-			<li><strong>12:00pm:</strong> Stations of the Cross</li>
-			<li><strong>1pm&ndash;3pm:</strong> Good Friday Liturgy</li>
-		</ul>
-		<div class="hr"><hr></div>
-		<h3>Holy Saturday, April 15th</h3>
-		<ul>
-			<li><strong>8:00pm:</strong> Vigil of Easter;<br>Lighting of Paschal Candle<br>&amp; First Eucharist of Easter</li>
-		</ul>
-		<div class="hr"><hr></div>
-		<h3>Easter Sunday, April 16th</h3>
-		<ul>
-			<li><strong>7:00am:</strong> Holy Eucharist, Rite I</li>
-			<li><strong>9:00am:</strong> Festival Choral<br>Eucharist, Rite II<br>
-				<em>Child Care provided for infants<br>and toddlers</em></li>
-			<li><strong>11:00am:</strong> Family &amp; Children&#8217;s Eucharist<br>
-				<em>Children stay in church with<br>their families</em><br>
-				<em>EASTER EGG HUNT FOLLOWS<br>THIS SERVICE</em></li>
-		</ul>
-	</div>
-	<!-- End Special Services -->
+    <div class="hr hr_large"><hr class="large_dots"></div>
 
-	<div class="hr hr_large"><hr class="large_dots"></div>
+    <!-- Memorial Garden Donations -->
+    <div id="memorial-garden">
+        <a href="https://e-giving.org/egivinglogin.asp?id=1435" target="_blank"><img src="/images/memorial_garden_logo.png" height="204" width="208"></a>
+    </div>
+    <!-- End Memorial Garden Donations -->
 
-	<!-- Memorial Garden Donations -->
-	<div id="memorial-garden">
-		<a href="https://e-giving.org/egivinglogin.asp?id=1435" target="_blank"><img src="/images/memorial_garden_logo.png" height="204" width="208"></a>
-	</div>
-	<!-- End Memorial Garden Donations -->
+    <div class="hr hr_large"><hr class="large_dots"></div>
 
-	<div class="hr hr_large"><hr class="large_dots"></div>
+    <!-- Featured Events -->
+    <div id="events">
+        <h2>Featured Events</h2>
+        <!-- <p>
+            No upcoming events.
+        </p> -->
+        <div class="side-bar-event">
+            <h3>Save The Dates</h3>
+            <ul class="info">
+                <li><strong>July 7th&ndash;9th</strong><br> St. Mary&#8217;s by-the-Sea • Diamond Jubilee • 60th Annual Antiques &amp; Collectibles Show</li>
+            </ul>
+        </div>
+        <!--<div class="side-bar-event">
+            <h3>Upcoming Concerts</h3>
+            <ul class="info">
+                <li><strong>June 18th @ 3:00PM</strong><br> Mari Black Trio&mdash;Special Father&#8217;s<br>Day Show</li>
+            </ul>
+        </div>-->
+        <p>
+            <a href="/newsandevents.html#featured">More event details</a> &bull;
+            <a href="/newsandevents.html#calendar">View calendar</a>
+        </p>
+    </div>
+    <!-- End Featured Events -->
 
-	<!-- Featured Events -->
-	<div id="events">
-		<h2>Featured Events</h2>
-		<p>
-			No upcoming events.
-		</p>
-		<!-- <div class="side-bar-event">
-			<h3>Upcoming Concerts</h3>
-			<ul class="info">
-				<li><strong>March 19th @ 3:00PM</strong><br> St. Mary&#8217;s <em>Party for Paddy</em> Concert</li>
-			</ul>
-		</div> -->
-		<!-- <div class="side-bar-event">
-			<h3>Save The Dates</h3>
-			<ul class="info">
-				<li><strong>July 8th&ndash;10th</strong><br> St. Mary&#8217;s by-the-Sea 59th Annual Antiques &amp; Collectibles Show</li>
-			</ul>
-		</div> -->
-		<p>
-			<a href="/newsandevents.html#featured">More event details</a> &bull;
-			<a href="/newsandevents.html#calendar">View calendar</a>
-		</p>
-	</div>
-	<!-- End Featured Events -->
+    <div class="hr hr_large"><hr class="large_dots"></div>
+    <!-- Newsletters -->
+    <div id="newsletters">
+        <h2>Monthly Newsletter</h2>
+        <p>
+            Check out our current <em>Times &amp; Tides Newsletter</em>.
+        </p>
+        <p>
+            <a href="/newsletter.html">View Newsletters &gt;</a>
+        </p>
+        <div class="newsletter-signup">
+            <h3>Subscribe to our eNewsletter!</h3>
+            <form method="post" action="http://oi.vresp.com?fid=3a256fc759" target="vr_optin_popup" onsubmit="window.open( 'http://www.verticalresponse.com', 'vr_optin_popup', 'scrollbars=yes,width=600,height=450' ); return true;" >
+                <p>
+                    <label>Email:</label>
+                    <input name="email_address" type="email" required />
+                </p>
+                <input type="submit" value="Join Now" class="button" />
+            </form>
+        </div>
+    </div>
+    <!-- End Newsletters -->
+    <div class="hr hr_large"><hr class="large_dots"></div>
 
-	<div class="hr hr_large"><hr class="large_dots"></div>
-	<!-- Newsletters -->
-	<div id="newsletters">
-		<h2>Monthly Newsletter</h2>
-		<p>
-			Check out our current <em>Times &amp; Tides Newsletter</em>.
-		</p>
-		<p>
-			<a href="/newsletter.html">View Newsletters &gt;</a>
-		</p>
-		<div class="newsletter-signup">
-			<h3>Subscribe to our eNewsletter!</h3>
-			<form method="post" action="http://oi.vresp.com?fid=3a256fc759" target="vr_optin_popup" onsubmit="window.open( 'http://www.verticalresponse.com', 'vr_optin_popup', 'scrollbars=yes,width=600,height=450' ); return true;" >
-				<p>
-					<label>Email:</label>
-					<input name="email_address" type="email" required />
-				</p>
-				<input type="submit" value="Join Now" class="button" />
-			</form>
-		</div>
-	</div>
-	<!-- End Newsletters -->
-	<div class="hr hr_large"><hr class="large_dots"></div>
-
-	<!-- Misc. Links -->
-	<div id="donate_buttons">
-		<a href="https://e-giving.org/egivinglogin.asp?id=1435"><img src="/images/donate.png" alt="Dontate!"></a>
-		<a href="/planned_giving.html"><img src="/images/planned_giving.png" alt="Planned Giving"></a>
-	</div>
-	<div class="hr hr_large"><hr class="large_dots"></div>
-	<div id="misc_side_bar">
-		<img src="/images/tec_welcomes.png" alt="The Episcopal Church Welcomes You">
-	</div>
-	<!-- End Misc. Links -->
+    <!-- Misc. Links -->
+    <div id="donate_buttons">
+        <a href="https://e-giving.org/egivinglogin.asp?id=1435"><img src="/images/donate.png" alt="Dontate!"></a>
+        <a href="/planned_giving.html"><img src="/images/planned_giving.png" alt="Planned Giving"></a>
+    </div>
+    <div class="hr hr_large"><hr class="large_dots"></div>
+    <div id="misc_side_bar">
+        <img src="/images/tec_welcomes.png" alt="The Episcopal Church Welcomes You">
+    </div>
+    <!-- End Misc. Links -->
 
 </div>
